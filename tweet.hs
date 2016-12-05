5494c01375659a67bcc229135f44b61207a9833c
nanstc <nanstchen@gmail.com>
2016-12-04 21:43:14 -0800

    completed program that handles both hashtag and handle

--- tweet.hs
+++ tweet.hs
@@ -4,52 +4,72 @@ import Data.ByteString (ByteString)
 import Network.HTTP.Conduit
 import Web.Authenticate.OAuth
 import Data.Aeson
+import Data.Aeson.Types
 import Data.Time.Clock (UTCTime)
 import Data.Text (Text)
+import Data.Maybe
+import Data.Char
 import GHC.Generics
 
--- Insert here your own credentials
+-- The Twitter API credentials of our app
 
 myoauth :: OAuth
-myoauth =
-  newOAuth { oauthServerName     = "api.twitter.com"
+myoauth = newOAuth 
+           { oauthServerName     = "api.twitter.com"
            , oauthConsumerKey    = "TEjYdE3Ac3s8UtWr3n6PVojFw"
            , oauthConsumerSecret = "qcc67ceJfSOd9x1aSx02nUIarGmmZDpzishy79gUl8ZxwhYgHq"
-             }
+           }
 
 mycred :: Credential
 mycred = newCredential "727254860375052289-ta0l05o8hwvycLDfdyc0loQPaaHUiQk"
                        "3WW4dDQMzpkHSOIVL3O7jRsZ3GHcZJjWdyf5KtTR3V5fc"
 
--- | Type for tweets. Use only the fields you are interested in.
---   The parser will filter them. To see a list of available fields
---   see <https://dev.twitter.com/docs/platform-objects/tweets>.
+-- For documentation on the availalbe Tweeter API
+-- fields used in parser please refer to 
+-- https://dev.twitter.com/docs/platform-objects/tweets
+
+-- data type of a tweet that we display on screen
 data Tweet =
   Tweet { text :: !Text
         , retweet_count :: !Integer
         , favorite_count :: !Integer
+        , created_at :: !String
           } deriving (Show, Generic)
+          
+instance FromJSON Tweet where
+ parseJSON (Object v) =
+    Tweet  <$> v .: "text"
+           <*> v .: "retweet_count"
+           <*> v .: "favorite_count"
+           <*> v .: "created_at"
 
-instance FromJSON Tweet
-instance ToJSON Tweet
+-- Parser to parse a list of tweets in json forms
+-- , specfically for hashtag handler
+tweets :: Value -> Parser [Tweet]
+tweets  = withObject "tweets" (.: "statuses")
 
+
+-- output tweet information on screen
 format :: Tweet -> IO()
 format tweet = do 
    putStrLn $ "Tweet: " ++ (show $ text tweet)
    putStrLn $ "Favourite count: " ++ (show $ favorite_count tweet)
    putStrLn $ "Retweet count: " ++ (show $ retweet_count tweet)
+   putStrLn $ "Posted at: " ++ (show $ created_at tweet)
    putStrLn $ ""
 
 
--- | This function reads a timeline JSON and parse it using the 'Tweet' type.
-timeline :: String -- ^ Screen name of the user
-         -> IO (Either String [Tweet]) -- ^ If there is any error parsing the JSON data, it
-                                       --   will return 'Left String', where the 'String'
-                                       --   contains the error information.
+-- A function that calls Tweeter API, receives 
+-- timeline JSON based on user handle, and calls
+-- function that parses the JSON to return tweets
+-- , or error if parser fails 
+timeline :: String -- the handle user is interested in
+         -> IO (Either String [Tweet]) -- left String = error message
+                                       -- right [Tweet] = proper output
 timeline name = do
-  -- Firstly, we create a HTTP request with method GET (it is the default so we don't have to change that).
+  -- create a HTTP request and binds it to req
   req <- parseUrl $ "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" ++ name
-  -- Using a HTTP manager, we authenticate the request and send it to get a response.
+  -- Use a HTTP manager to authenticate the request, and send it to get a response.
   res <- withManager $ \m -> do
            -- OAuth Authentication. 'signOAuth' modifies the HTTP header adding the
            -- appropriate authentication.
@@ -58,23 +78,85 @@ timeline name = do
            httpLbs signedreq m
   -- Decode the response body.
   return $ eitherDecode $ responseBody res
+  
+  
+-- A function that calls Tweeter API, receives 
+-- hashtag search result JSON based on hashtag, 
+-- and calls function that parses the JSON to 
+-- return tweets, or error if parser fails 
 
+searchResult :: String -- the hashtag user is interested in
+         -> String
+         -> IO (Either String [Tweet]) -- left String = error message
+                                       -- right [Tweet] = proper output
+         
+searchResult name counts = do
+  -- create a HTTP request and binds it to req
+  req <- parseUrl $ "https://api.twitter.com/1.1/search/tweets.json?q=%23" ++ name ++ "&result_type=popular&count="++counts
+  -- Use a HTTP manager to authenticate the request, and send it to get a response.
+  res <- withManager $ \m -> do
+           -- OAuth Authentication. 'signOAuth' modifies the HTTP header adding the
+           -- appropriate authentication.
+           signedreq <- signOAuth myoauth mycred req
+           -- Send request.
+           httpLbs signedreq m
+  -- Decode the response body.
+  -- Set error message as Left variable
+  let err = Left "Cannot parse the result from search." :: Either String [Tweet]
+  -- Parse array of twitter in the "statuses" tag in JSON and set as Right variable
+  let twts = Right $ fromJust $ parseMaybe tweets  =<< decode (responseBody res) :: Either String [Tweet]
+  return twts
+  
+
+-- A simple function to get rid of leading '@' in input, if at all 
+parsehandle [] = []
+parsehandle (h:t)
+    | h == '@' = parsehandle t
+    | otherwise = h:parsehandle t
+    
+-- A simple function to get rid of leading '#' in input, if at all 
+parsehashtag [] = []
+parsehashtag (h:t)
+    | h == '#' = parsehashtag t
+    | otherwise = h:parsehashtag t
+    
+-- The function that gives back tweets based on handles 
 handle_handling :: IO ()
 handle_handling = do 
-    putStrLn "Enter a handle you would like to search, without the '@'"
-    -- Read the timeline of an inputted handle. 
+    putStrLn "Enter a handle you would like to search:"
+    h <- getLine
+    putStrLn "What is the maximum number of tweets you would like to see?"
     n <- getLine
-    ets <- timeline n
+    -- Calls the timeline function to get formatted tweets
+    ets <- timeline $ parsehandle h
+    putStrLn $ ""
+    putStrLn $ "Searched handle: @" ++ h
+    putStrLn $ ""
     case ets of
         -- When the parsing of the JSON data fails, we report it.
         Left err -> putStrLn err
-   -- When successful, print in the screen the first 5 tweets. mapM_ print $ take 1 ts
-        Right ts  -> mapM_ format $ take 5 ts
+        -- When successful, print in the screen the number of tweets user specified
+        Right ts  -> mapM_ format $ take (read n :: Int) ts
 
+-- The function that gives back tweets based on hashtags 
 hashtag_handling :: IO ()
 hashtag_handling = do 
-    putStrLn "This function is not yet implemented!"
+    putStrLn "Enter the hashtag you would like to look up:"
+    h <- getLine
+    putStrLn "What is the maximum number of tweets you would like to see?"
+    n <- getLine
+    -- Calls the searchResult function to get formatted tweets
+    ets <- searchResult (parsehashtag h) n
+    putStrLn $ ""
+    putStrLn $ "Searched hashtag: #" ++ h
+    putStrLn $ ""
+    case ets of 
+        -- When the parsing of the JSON data fails, we report it.
+        Left err -> putStrLn err
+        -- When successful, print in the screen the number of tweets user specified
+        Right ts -> mapM_ format $ take (read n :: Int) ts
 
+-- Function that greets users when entering system
 greeting :: IO ()
 greeting = do 
   putStrLn "Welcome to Tweetmeet! This mini-app provides you live tweets of your faviourite subject topics."
